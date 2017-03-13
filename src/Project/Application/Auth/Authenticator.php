f55a7a610cc99d5910321473bcf61dcf57a1cd98
Peter Aba <peter@peteraba.com>
2017-03-13 03:13:14 +0100

    Authentication works

--- src/Project/Application/Auth/Authenticator.php
+++ src/Project/Application/Auth/Authenticator.php
@@ -2,6 +2,7 @@
 
 namespace Project\Application\Auth;
 
+use Opulence\Cryptography\Hashing\BcryptHasher;
 use Opulence\Databases\ConnectionPools\ConnectionPool;
 use Opulence\QueryBuilders\MySql\QueryBuilder;
 
@@ -10,31 +11,55 @@ class Authenticator
     /** @var ConnectionPool */
     protected $connectionPool;
 
+    /** @var BcryptHasher */
+    protected $bcryptHasher;
+
     /**
      * Authenticator constructor.
      *
      * @param ConnectionPool $connectionPool
+     * @param BcryptHasher $bcryptHasher
      */
-    public function __construct(ConnectionPool $connectionPool)
+    public function __construct(ConnectionPool $connectionPool, BcryptHasher $bcryptHasher)
     {
         $this->connectionPool = $connectionPool;
+        $this->bcryptHasher = $bcryptHasher;
     }
 
     /**
      * @param string $username
-     * @param string $password
+     * @param string $oassword
      *
      * @return bool
      */
-    public function canLogin(string $username, string $password): bool
+    public function canLogin(string $username, string $oassword): bool
+    {
+        $salt = getenv('ENCRYPTION_KEY');
+
+        $hashedValue = $this->getUserPassword($username);
+        if (empty($hashedValue)) {
+            return false;
+        }
+
+        $hashedValue = $this->bcryptHasher->hash($oassword, [], $salt);
+
+        $verified = $this->bcryptHasher->verify($hashedValue, $oassword, $salt);
+
+        return $verified;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return string
+     */
+    public function getUserPassword(string $username) :string
     {
         $readConnection = $this->connectionPool->getReadConnection();
 
-        $query = (new QueryBuilder())->select('id')
+        $query = (new QueryBuilder())->select('password')
             ->from('users')
             ->where('username = :username')
-//            ->andWhere('password = :password')
-//            ->addNamedPlaceholderValue('password', $password)
             ->addNamedPlaceholderValue('username', $username);
 
         $statement = $readConnection->prepare($query->getSql());
@@ -42,8 +67,11 @@ class Authenticator
         $statement->execute();
 
         $row = $statement->fetch(\PDO::FETCH_ASSOC);
+        if (!isset($row['password'])) {
+            return '';
+        }
 
-        return !empty($row);
+        return $row['password'];
     }
 }
 
